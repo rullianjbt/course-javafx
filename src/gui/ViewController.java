2bf27dd0ce461a67055089946b89c9048ccf007e
Rullian Siqueira da Mata <rullianjbt@gmail.com>
2021-03-15 10:30:25 -0300

    Changing the view and controller to calculate Sum

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,19 +1,41 @@
 package gui;
 
+import java.util.Locale;
+
 import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 
 public class ViewController {
 
 	@FXML
-	private Button btTest;
+	private TextField txtNumber1;
 
 	@FXML
-	public void onBtTestAction() {
-		Alerts.showAlert("Alert title", "Alert header", "Hello", AlertType.INFORMATION);
-		;
+	private TextField txtNumber2;
+
+	@FXML
+	private Label labelResult;
+
+	@FXML
+	private Button btSum;
+
+	@FXML
+	public void onBtSumAction() {
+		try {
+			Locale.setDefault(Locale.US);
+			double number1 = Double.parseDouble(txtNumber1.getText());
+			double number2 = Double.parseDouble(txtNumber2.getText());
+			double sum = number1 + number2;
+			labelResult.setText(String.format("%.2f", sum));
+		}
+		catch(NumberFormatException e) {
+			Alerts.showAlert("Error", "Parse error", e.getMessage(), AlertType.ERROR);
+		}
+
 	}
 
 }
